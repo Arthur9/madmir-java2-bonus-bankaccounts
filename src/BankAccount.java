8318f631c9f46ea2abcfe40205db9c435eb23215
Arthur9 <jawzproductions@hotmail.com>
2013-08-23 13:27:11 -0400

    Here is my Bonus Bank Account Project

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,4 +1,45 @@
 
 public class BankAccount {
 
+	private int customerAccNumber;
+	private float customerName;
+	private double customerAccBalance;
+
+
+//Overloaded Constructor
+public void Customer (int accNum, float custName, int accBal){
+	this.customerAccNumber = accNum;
+	this.customerName = custName;
+	this.customerAccBalance = accBal;
+	
+}
+
+//Default constructor
+public BankAccount() {
+	this(0,0,0.0));
+}
+
+//Accessor Methods
+public int getCustomerAccNumber(){
+	return customerAccNumber;
+}
+
+public float getCustomerName(){
+	return customerName;
+}
+
+public double getCustomerAccBalance(){
+	return customerAccBalance;
+}
+
+//Mutator Methods
+public void setCustomerAccNumber(int newAccNum){
+	customerAccNumber = newAccNum;
+}
+public void setCustomerName(float newCustName){
+	customerName = newCustName;
+}
+public void setCustomerAccBalance(double newAccBal){
+	customerAccBalance = newAccBal;
+}
 }
--- src/TestBankAccount.java
+++ src/TestBankAccount.java
@@ -1,7 +1,19 @@
 
 public class TestBankAccount {
 
+	//This is the Test for Bank Account
 	public static void main(String[] args) {
+	
+		BankAccount billGates = new BankAccount(0,"0",0.0);
+		Customer billGates = new Customer(4650,billGatesCustomerName,556789345.00);
+		Customer markZuckerberg = new Customer(550665734000.00);
+		Customer arthurSimon = new Customer();
+		
+		billGates.setAccNum(4651);
+		billGates.setAccBal(99550555000.00);
+		
+		System.out.println(" Bill Gate's Account Number is " + billGates.getCustomerAccNumber() + " and his Account Balance is " + billGates.getCustomerAccBalance());
+		System.out.println(" Mark Zuckerbert's Account Number is " + markZuckerberg.getCustomerAccNumber() + " and his Account Balance is " + markZuckerberg.getCustomerAccBalance());
 		
 		
 	}
